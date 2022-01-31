dfe2c93fbc2570b11c79e049b1bf408d29268f73
farhansadiqmullick <farhansadiq24@gmail.com>
2022-01-31 20:39:24 +0600

    ajax portfolio developed

--- ajax-portfolio.php
+++ ajax-portfolio.php
@@ -11,7 +11,8 @@ Text Domain: ajax-port
 Domain Path: /languages/
 */
 require_once "class.datatable.php";
-class AJAX_PORT{
+class AJAX_PORT
+{
 
     public function __construct()
     {
@@ -20,63 +21,70 @@ class AJAX_PORT{
         add_action('wp_enqueue_scripts', array($this, 'frontend_assets_load'));
         add_action('init', array($this, 'register_cpt'));
         add_shortcode('portfolio_items', array($this, 'display_portfolio'));
-        
+
         //AJAX loading
         add_action('wp_ajax_ajax-portfolio', array($this, 'ajax_portfolio_fetch'));
         add_action('wp_ajax_nopriv_ajax-portfolio',  array($this, 'ajax_portfolio_fetch'));
 
-         //hooks for display columns
+        //hooks for display columns
         add_action('manage_posts_columns', array($this, 'col_sort_manage_post_column'));
         add_action('manage_posts_custom_column', array($this, 'col_sort_manage_custom_column'), 10, 2);
     }
 
-    function load_ajax_port_textdomain(){
-        load_plugin_textdomain( 'ajax-port', false, plugin_dir_path( __FILE__ ).'/languages');
+    function load_ajax_port_textdomain()
+    {
+        load_plugin_textdomain('ajax-port', false, plugin_dir_path(__FILE__) . '/languages');
     }
 
-    function plugin_assets_load(){
+    function plugin_assets_load()
+    {
     }
-    
-    function frontend_assets_load(){
+
+    function frontend_assets_load()
+    {
         global $wp_query;
         $ajaxurl = admin_url("admin-ajax.php");
-        wp_enqueue_style( 'style-css', plugin_dir_url( __FILE__ ).'assets/css/style.css', null, rand(111,999), 'all');
-        wp_enqueue_script( 'main-js', plugin_dir_url( __FILE__ ).'assets/js/main.js', ['jquery'], rand(111,999), true);
-        wp_localize_script( 'main-js', 'portfolio_fetch', array(
-            'ajaxurl' => $ajaxurl,
-            'category' => get_the_category( ),
-        ),
-    );
+        wp_enqueue_style('style-css', plugin_dir_url(__FILE__) . 'assets/css/style.css', null, rand(111, 999), 'all');
+        wp_enqueue_script('main-js', plugin_dir_url(__FILE__) . 'assets/js/main.js', ['jquery'], rand(111, 999), true);
+        wp_localize_script(
+            'main-js',
+            'portfolio_fetch',
+            array(
+                'ajaxurl' => $ajaxurl,
+                'category' => get_the_category(),
+            ),
+        );
     }
 
-    function register_cpt(){
+    function register_cpt()
+    {
         $labels = array(
-			"name"               => __("Portfolio", "ajax-port"),
-			"singular_name"      => __("Portfolio", "ajax-port"),
-		);
+            "name"               => __("Portfolio", "ajax-port"),
+            "singular_name"      => __("Portfolio", "ajax-port"),
+        );
 
-		$args = array(
-			"label"               => __("Portfolio", "ajax-port"),
-			"labels"              => $labels,
-			"description"         => "",
-			"public"              => false,
-			"publicly_queryable"  => true,
-			"show_ui"             => true,
+        $args = array(
+            "label"               => __("Portfolio", "ajax-port"),
+            "labels"              => $labels,
+            "description"         => "",
+            "public"              => false,
+            "publicly_queryable"  => true,
+            "show_ui"             => true,
             'taxonomies'          => array('category', 'post_tag'),
-			"delete_with_user"    => false,
-			"show_in_rest"        => true,
-			"has_archive"         => true,
-			"show_in_menu"        => true,
-			"show_in_nav_menus"   => false,
-			"capability_type"     => "post",
-			"map_meta_cap"        => true,
-			"hierarchical"        => false,
-			"rewrite"             => array("slug" => "portfolio", "with_front" => true),
-			"query_var"           => true,
-            "supports"           => array('title', "excerpt", "page-attributes",'thumbnail', "custom-fields"),
-		);
+            "delete_with_user"    => false,
+            "show_in_rest"        => true,
+            "has_archive"         => true,
+            "show_in_menu"        => true,
+            "show_in_nav_menus"   => false,
+            "capability_type"     => "post",
+            "map_meta_cap"        => true,
+            "hierarchical"        => false,
+            "rewrite"             => array("slug" => "portfolio", "with_front" => true),
+            "query_var"           => true,
+            "supports"           => array('title', "excerpt", "page-attributes", 'thumbnail', "custom-fields"),
+        );
 
-		register_post_type("portfolio", $args);
+        register_post_type("portfolio", $args);
     }
 
     function col_sort_manage_post_column($columns)
@@ -100,7 +108,8 @@ class AJAX_PORT{
         }
     }
 
-    function display_portfolio(){
+    function display_portfolio()
+    {
 
         $args = [
             'post_type' => 'portfolio',
@@ -109,21 +118,22 @@ class AJAX_PORT{
         ];
 
         $portfolio_query = new WP_Query($args);
-       $portfolio = [];
+        $portfolio = [];
 
-        while($portfolio_query->have_posts()){ $portfolio_query->the_post();
+        while ($portfolio_query->have_posts()) {
+            $portfolio_query->the_post();
             $portfolio[] = array(
                 'title' => get_the_title(get_the_ID()),
-                'link' => get_the_permalink( get_the_ID()),
+                'link' => get_the_permalink(get_the_ID()),
                 'id' => get_the_ID(),
                 'excerpt' => get_the_excerpt(get_the_ID()),
-                'image' => get_the_post_thumbnail_url(get_the_ID(), array(300,300)),
-                'price' => get_post_meta( get_the_ID(), 'price'),
-                'category' => get_the_category( get_the_ID() ),
+                'image' => get_the_post_thumbnail_url(get_the_ID(), array(300, 300)),
+                'price' => get_post_meta(get_the_ID(), 'price'),
+                'category' => get_the_category(get_the_ID()),
             );
         }
         wp_reset_query();
-        ?>
+?>
         <div class="menu">
             <!--title-->
             <div class="title">
@@ -133,26 +143,28 @@ class AJAX_PORT{
             <!--Filter Buttons-->
             <div class="btn-container">
                 <button type="button" class="filter-btn" data-category="all">All</button>
-                   <?php for($i=0; $i<count($portfolio); $i++){
-                echo '<button type="button" class="filter-btn" data-category="'.$portfolio[$i]['category'][0]->category_nicename.'">'.$portfolio[$i]['category'][0]->name.'</button>'.'</br>';
-                    } ?>
+                <?php for ($i = 0; $i < count($portfolio); $i++) {
+                    echo '<button type="button" class="filter-btn" data-category="' . $portfolio[$i]['category'][0]->category_nicename . '">' . $portfolio[$i]['category'][0]->name . '</button>' . '</br>';
+                } ?>
             </div>
             <div class="portfolio_load"></div>
             <!--menu items-->
             <div class="section-center">
-                <?php for($i=0; $i<count($portfolio); $i++): ?>
-                <article class="menu-item">
-                    <a class="portfolio-image" href="<?php echo $portfolio[$i]['link']; ?>" data-id="<?php echo $portfolio[$i]['id'] ?>"><img src="<?php echo $portfolio[$i]['image'] ?>" class="photo" alt="image-1"></a>
-                    <div class="item-info">
-                        <header>
-                            <a class="portfolio-image" href="<?php echo $portfolio[$i]['link']; ?>"><h4><?php echo $portfolio[$i]['title'];?></h4></a>
-                            <h4 class="price"><?php echo $portfolio[$i]['price'][0] ?></h4>
-                        </header>
-                        <p class="item-text">
-                            <?php echo $portfolio[$i]['excerpt']; ?>
-                        </p>
-                    </div>
-                </article>
+                <?php for ($i = 0; $i < count($portfolio); $i++) : ?>
+                    <article class="menu-item" id="<?php echo $portfolio[$i]['category'][0]->category_nicename; ?>">
+                        <a class="portfolio-image" href="<?php echo $portfolio[$i]['link']; ?>" data-id="<?php echo $portfolio[$i]['id'] ?>"><img src="<?php echo $portfolio[$i]['image'] ?>" class="photo" alt="image-1"></a>
+                        <div class="item-info">
+                            <header>
+                                <a class="portfolio-image" href="<?php echo $portfolio[$i]['link']; ?>" data-id="<?php echo $portfolio[$i]['id'] ?>">
+                                    <h4><?php echo $portfolio[$i]['title']; ?></h4>
+                                </a>
+                                <h4 class="price"><?php echo $portfolio[$i]['price'][0] ?></h4>
+                            </header>
+                            <p class="item-text">
+                                <?php echo $portfolio[$i]['excerpt']; ?>
+                            </p>
+                        </div>
+                    </article>
                 <?php endfor; ?>
             </div>
         </div>
@@ -160,36 +172,38 @@ class AJAX_PORT{
     }
 
 
-    function ajax_portfolio_fetch(){
+    function ajax_portfolio_fetch()
+    {
         $args = [
             'posts_per_page' => '1',
-            'p' => intval( $_POST['post_id'] ),
-            'post_type' => get_post_type( $_POST['post_id'] ),
+            'p' => intval($_POST['post_id']),
+            'exit' => $_POST['exit'],
+            'post_type' => get_post_type($_POST['post_id']),
         ];
 
-    // it is always better to use WP_Query but not here
-    $query = new WP_Query($args);
-    if ($query->have_posts()) :
-       while ($query->have_posts()) : $query->the_post(); ?>
-    <article class="menu-item">
-         <a class="portfolio-image" href="<?php echo get_the_permalink( get_the_ID()); ?>"><img src="<?php echo get_the_post_thumbnail_url( get_the_ID()); ?>" class="photo" alt="Portfolio Image"></a>
-        <div class="item-info">
-            <header>
-                <h4><?php echo get_the_title(get_the_ID());?></h4>
-                <h4 class="price"><?php echo get_post_meta(get_the_ID(), 'price')[0]; ?></h4>
-            </header>
-            <p class="item-text">
-                <?php echo get_the_excerpt( get_the_ID()); ?>
-            </p>
-        </div>
-   </article>
-  <?php   
-    endwhile; endif;
-    wp_reset_postdata();
-    die();
-
-        }
-    
+        // it is always better to use WP_Query but not here
+        $query = new WP_Query($args);
+        if ($query->have_posts()) :
+            while ($query->have_posts()) : $query->the_post(); ?>
+                    <div class="ajax_item">
+                        <a class="portfolio-image" href="<?php echo get_the_permalink(get_the_ID()); ?>"><img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" class="photo" alt="Portfolio Image"></a>
+                        <div class="item-info">
+                            <header>
+                            <a class="portfolio-image" href="<?php echo get_the_permalink(get_the_ID());?>" data-id="<?php echo get_the_ID(); ?>"><h4><?php echo get_the_title(get_the_ID()); ?></h4></a>
+                                <h4 class="price"><?php echo get_post_meta(get_the_ID(), 'price')[0]; ?></h4>
+                            </header>
+                            <p class="item-text">
+                                <?php echo get_the_excerpt(get_the_ID()); ?>
+                            </p>
+                        </div>
+                    </div>
+                
+<?php
+            endwhile;
+        endif;
+        wp_reset_postdata();
+        die();
+    }
 }
 
 new AJAX_PORT();
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@
             e.preventDefault();
            data = {
                'action' : 'ajax-portfolio',
-               'post_id': $(this).data('id')
-
+               'post_id': $(this).data('id'),
+               
            };
            console.log(data);
 
@@ -16,9 +16,37 @@
                 beforeSend: function () {
                     $(this).text('Loading...'); // change the button text, you can also add a preloader image
                 },
-                success: function (data) {              
-                    $('.portfolio_load').hide().html(data).fadeIn('slow');;
-                }
+                success: function (data) { 
+                    $('.portfolio_load').hide().html(data).fadeIn('slow');
+                    $('body').addClass('overlay');
+                    $('.portfolio_load').on('click',function(){
+                        $('.portfolio_load').hide();
+                        $('body').removeClass('overlay');
+                    });            
+            },
+
+
+            });
+
+            $('.close_button').on('click', function(){
+                console.log('closed');
+                
+            });
+
+        });
+
+
+        $(".filter-btn").map(function(index, btn){
+            $(btn).on('click', function(e){
+               let category =  $(e.currentTarget).data('category');
+
+               if(category == 'all'){
+                   $(".menu-item").fadeOut(0);
+                   $(".menu-item").fadeIn(1000);
+               }else{
+                   $(".menu-item").hide();
+                   $(".menu-item"+"#"+category).fadeIn(1000);
+               }
             });
         });
     });
